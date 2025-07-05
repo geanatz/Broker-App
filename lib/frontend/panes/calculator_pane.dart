ed4c5e56d2d57dabc2df7db9bba1fcef3f9bdce3
geanatz <geanatz@protonmail.com>
2025-07-05 23:31:42 +0300

    A doua versiune

--- lib/frontend/panes/calculator_pane.dart
+++ lib/frontend/panes/calculator_pane.dart
@@ -476,7 +476,7 @@ class _CalculatorPaneState extends State<CalculatorPane> {
                           children: [
                             // Monthly Payment
                             LightItem3(
-                              title: 'Rata lunara',
+                              title: 'Rata aviatocco',
                               description: _monthlyPayment > 0 ? _monthlyPayment.toStringAsFixed(1) : '0.0',
                               backgroundColor: AppTheme.containerColor1,
                               titleColor: AppTheme.elementColor2,
